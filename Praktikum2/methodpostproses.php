8a3ef564cff1d01ecdc5697098cc92f60eed7f88
Nrfashi <nrfashiha892@gmail.com>
2025-12-10 11:24:31 +0700

    Add files via upload

---
+++ Praktikum2/methodpostproses.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title> Method POST proses </title>
+</head>
+<body>
+Data nama yang diinputkan adalah : <?echo $_POST["Nur_Fashihatul_Jannah"]?>
+</body>
+</html>
---
+++ Praktikum2/metodegetproses.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Method GET Proses</title>
+</head>
+<body>
+    Data nama yang diinputkan adalah: <?echo $_GET["Nur_Fashihatul_Jannah"]; ?>
+</body>
+</html>
