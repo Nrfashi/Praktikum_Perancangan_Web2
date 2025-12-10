c122ac7d6463e5d5ff5a94ac7f46a64198ddaab0
Nrfashi <nrfashiha892@gmail.com>
2025-12-10 11:26:56 +0700

    Delete Praktikum2 directory

--- Praktikum2/methodpostproses.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-	<title> Method POST proses </title>
-</head>
-<body>
-Data nama yang diinputkan adalah : <?echo $_POST["Nur_Fashihatul_Jannah"]?>
-</body>
-</html>
--- Praktikum2/metodegetproses.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>Method GET Proses</title>
-</head>
-<body>
-    Data nama yang diinputkan adalah: <?echo $_GET["Nur_Fashihatul_Jannah"]; ?>
-</body>
-</html>
